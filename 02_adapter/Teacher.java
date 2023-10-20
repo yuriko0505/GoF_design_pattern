9f5f4f66000914bf18cd47537b76020260f6032a
Yuriko Yamauchi <uitolp-tge999-hykook@g.ecc.u-tokyo.ac.jp>
2023-10-20 21:51:04 +0900

    learned Adapter

---
+++ 02_adapter/Teacher.java
@@ -0,0 +1,7 @@
+public class Teacher{
+	public static void main(String args[]){
+		// Chairperson chairperson = new NewTaro();
+		Chairperson chairperson = new Hanako();
+		chairperson.organizeClass();
+	}
+}
