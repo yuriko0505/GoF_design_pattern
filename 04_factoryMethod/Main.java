ed1ed72312e310ffdba6f1303c6b7b54d8b5c73e
Yuriko Yamauchi <uitolp-tge999-hykook@g.ecc.u-tokyo.ac.jp>
2023-10-22 00:30:20 +0900

    learned FactoryMethod

---
+++ 04_factoryMethod/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String args[]){
+        ImagawasCutPrint cutPrint = new ImagawasCutPrint();
+        cutPrint.createCutPrint();
+    }
+}
---
+++ 04_factoryMethod/Potato.java
@@ -0,0 +1,8 @@
+public class Potato implements Cuttable{
+    public Potato(){
+        System.out.println("芋を版材にしました");
+    }
+    public void cut(){
+        System.out.println("芋を彫ります");
+    }
+}
